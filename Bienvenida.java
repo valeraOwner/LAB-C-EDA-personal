376f96cb8a552dd94b086084c2a4a76194101716
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-05-07 14:47:55 -0500

    Se corrigio error en el archivo  Bienvenida

--- Bienvenida.java
+++ Bienvenida.java
@@ -1,6 +1,6 @@
 
 public class Bienvenida{
-  public static void main(String[]args{
+  public static void main(String[]args){
     System.out.println("Bienvenida al curso de Laboratorio EDA - GRUPO C");
   }
 }
